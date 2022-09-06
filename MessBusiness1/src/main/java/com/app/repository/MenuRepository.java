a039ad7b2d2172de3e820e54ac0c9dd634e2df85
RMNH99 <112767589+RMNH99@users.noreply.github.com>
2022-09-06 09:58:04 +0530

    Delete MessBusiness1 directory

--- MessBusiness1/src/main/java/com/app/repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.model.MessMenu;
-
-public interface MenuRepository extends JpaRepository<MessMenu,String> {
-
-}
